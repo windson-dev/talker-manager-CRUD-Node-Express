69beb2400fd64c83881d4aff785c293ca7fcc636
Windson Macedo <windsondev@hotmail.com>
2022-11-04 16:33:29 -0300

    requisito 4

--- src/index.js
+++ src/index.js
@@ -62,12 +62,30 @@ function makeid(length) {
   return result;
 }
 
-app.post('/login/', (req, res) => {
+function validateLogin(req, res, next) {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).send({ message: 'O campo "email" é obrigatório' });
+  } 
+  if (!password) {
+    return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  if (email !== 'email@email.com') {
+    return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  return next();
+}
+
+app.post('/login/', validateLogin, (req, res) => {
     const { email, password } = req.body;
     const test = {
       email,
       password,
     };
+
     if (test) {
       return res.status(200).json({ token: makeid(16) });
     }
