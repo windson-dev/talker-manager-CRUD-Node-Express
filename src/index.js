5cdae9f640a4316428c663d9eebfc9c55a5281d9
Windson Macedo <windsondev@hotmail.com>
2022-11-04 19:23:38 -0300

    requisito 5

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ async function readFile() {
   }
 }
 
-app.get('/talker/', async (_req, res) => {
+app.get('/talker', async (_req, res) => {
   try {
     const talkers = await readFile();
     res.status(200).json(talkers);
@@ -64,6 +64,7 @@ function makeid(length) {
 
 function validateLogin(req, res, next) {
   const { email, password } = req.body;
+  const regexEmail = /\S+@\S+.\S+/;
   if (!email) {
     return res.status(400).send({ message: 'O campo "email" é obrigatório' });
   } 
@@ -73,13 +74,14 @@ function validateLogin(req, res, next) {
   if (password.length < 6) {
     return res.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
-  if (email !== 'email@email.com') {
+  if (!regexEmail.test(email)) {
     return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   return next();
 }
 
-app.post('/login/', validateLogin, (req, res) => {
+app.post('/login', validateLogin, (req, res) => {
+  try {
     const { email, password } = req.body;
     const test = {
       email,
@@ -89,4 +91,81 @@ app.post('/login/', validateLogin, (req, res) => {
     if (test) {
       return res.status(200).json({ token: makeid(16) });
     }
+  } catch (error) {
+    return res.status(404).send({ message: error.message });
+  }
+});
+
+function validateTalkerNameAge(req, res, next) {
+  const { name, age } = req.body;
+
+  if (!name) {
+    return res.status(400).send({ message: 'O campo "name" é obrigatório' });
+  } 
+  if (name.length < 3) {
+    return res.status(400).send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  } 
+  if (!age) {
+    return res.status(400).send({ message: 'O campo "age" é obrigatório' });
+  } 
+  if (age < 18) {
+    return res.status(400).send({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  return next();
+}
+
+function validateTalkerTalk(req, res, next) {
+  const { talk } = req.body;
+  const regexDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+  if (!talk) {
+    return res.status(400).send({ message: 'O campo "talk" é obrigatório' });
+  }
+  if (!talk.watchedAt) {
+    return res.status(400).send({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+
+  if (!regexDate.test(talk.watchedAt)) {
+    return res.status(400).send({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (!talk.rate) {
+    return res.status(400).send({ message: 'O campo "rate" é obrigatório' });
+  }
+  return next();
+}
+
+function validateTalkerRate(req, res, next) {
+  const { talk } = req.body;
+
+  if (!(talk.rate >= 1 && talk.rate <= 5 && talk.rate % 1 === 0)) {
+    return res.status(400).send({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  return next();
+}
+
+function isValidToken(req, res, next) {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).send({ message: 'Token não encontrado' });
+  }
+  if (authorization.length !== 16 || authorization instanceof String) {
+    return res.status(401).send({ message: 'Token inválido' });
+  }
+  return next();
+}
+
+app.post('/talker', isValidToken, validateTalkerNameAge, 
+  validateTalkerTalk, validateTalkerRate, async (req, res) => {
+  try {
+    const talkers = await readFile();
+    const newTalker = {
+      id: talkers[talkers.length - 1].id + 1,
+      ...req.body,
+    };
+    const allTalkers = JSON.stringify([...talkers, newTalker]);
+    await fs.writeFile(talkerPath, allTalkers);
+    res.status(201).json(newTalker);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
 });
