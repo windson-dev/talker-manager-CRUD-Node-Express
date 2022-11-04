f630cdc885f519c0dfb9035896db70ea9343517e
Windson Macedo <windsondev@hotmail.com>
2022-11-04 14:55:27 -0300

    requisito 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,11 +10,30 @@ const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
-// iniciando projeto
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+const talkerPath = path.resolve(__dirname, './talker.json');
+
+async function readFile() {
+  try {
+    const data = await fs.readFile(talkerPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`file not found ${error}`);
+  }
+}
+
+app.get('/talker/', async (_req, res) => {
+  try {
+    const talkers = await readFile();
+    res.status(200).json(talkers);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
